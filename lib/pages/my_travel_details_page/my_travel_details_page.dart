adb718b98f5377f13a143f4c3dde38ea036473f8
AmiK2001 <Amir.Niyazov.2001.AN@gmail.com>
2021-05-30 00:25:16 +0300

    added shitty map

--- lib/pages/my_travel_details_page/my_travel_details_page.dart
+++ lib/pages/my_travel_details_page/my_travel_details_page.dart
@@ -1,13 +1,14 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:latlng/latlng.dart';
 import 'package:travelplanner/models/gen/travel/locations.dart';
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart' as osm;
 import 'package:velocity_x/velocity_x.dart';
 
 import 'package:travelplanner/models/gen/travel/travel.dart';
 import 'package:travelplanner/pages/widgets/tp_text_field.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:travelplanner/utilities/constraints.dart';
-import 'package:map/map.dart';
+import 'package:map/map.dart' as flutter_map;
 
 class LocationEditForm extends StatefulWidget {
   final Locations location;
@@ -96,11 +97,6 @@ class _MyTravelDetailsPageState extends State<MyTravelDetailsPage>
         .doc(travelId)
         .snapshots();
   }
-
-  osm.MapController controller = osm.MapController(
-    initMapWithUserPosition: false,
-    initPosition: osm.GeoPoint(latitude: 47.435805, longitude: 8.48338),
-  );
 
   @override
   Widget build(BuildContext context) {
@@ -197,7 +193,10 @@ class _MyTravelDetailsPageState extends State<MyTravelDetailsPage>
                             .padding(const EdgeInsets.all(10))
                             .make()
                             .card
-                            .make();
+                            .make()
+                            .onTap(() {
+                          showMap();
+                        });
                       },
                       itemCount: travel.locations!.length,
                     )
@@ -216,27 +215,7 @@ class _MyTravelDetailsPageState extends State<MyTravelDetailsPage>
                                 .margin(const EdgeInsets.only(right: 6))
                                 .make(),
                             goodie.name!.text.bold.make().click(() {
-                              map();
-                              osm.OSMFlutter(
-                                controller: controller,
-                                road: osm.Road(
-                                  startIcon: osm.MarkerIcon(
-                                    icon: const Icon(
-                                      Icons.person,
-                                      size: 64,
-                                      color: Colors.brown,
-                                    ),
-                                  ),
-                                  roadColor: Colors.yellowAccent,
-                                ),
-                                markerIcon: osm.MarkerIcon(
-                                  icon: const Icon(
-                                    Icons.person_pin_circle,
-                                    color: Colors.blue,
-                                    size: 56,
-                                  ),
-                                ),
-                              );
+                              showMap();
                             }).make()
                           ],
                         )
@@ -263,6 +242,138 @@ class _MyTravelDetailsPageState extends State<MyTravelDetailsPage>
               ),
             );
           }),
+    );
+  }
+
+  final controller = flutter_map.MapController(
+    location: LatLng(35.68, 51.41),
+  );
+
+  final markers = [
+    LatLng(35.674, 51.41),
+    LatLng(35.676, 51.41),
+    LatLng(35.678, 51.41),
+    LatLng(35.68, 51.41),
+    LatLng(35.682, 51.41),
+    LatLng(35.684, 51.41),
+    LatLng(35.686, 51.41),
+  ];
+
+  void _gotoDefault() {
+    controller.center = LatLng(35.68, 51.41);
+    setState(() {});
+  }
+
+  void _onDoubleTap() {
+    controller.zoom += 0.5;
+    setState(() {});
+  }
+
+  Offset? _dragStart;
+  double _scaleStart = 1.0;
+  void _onScaleStart(ScaleStartDetails details) {
+    _dragStart = details.focalPoint;
+    _scaleStart = 1.0;
+  }
+
+  void _onScaleUpdate(ScaleUpdateDetails details) {
+    final scaleDiff = details.scale - _scaleStart;
+    _scaleStart = details.scale;
+
+    if (scaleDiff > 0) {
+      controller.zoom += 0.02;
+      setState(() {});
+    } else if (scaleDiff < 0) {
+      controller.zoom -= 0.02;
+      setState(() {});
+    } else {
+      final now = details.focalPoint;
+      final diff = now - _dragStart!;
+      _dragStart = now;
+      controller.drag(diff.dx, diff.dy);
+      setState(() {});
+    }
+  }
+
+  Widget _buildMarkerWidget(Offset pos, Color color) {
+    return Positioned(
+      left: pos.dx - 16,
+      top: pos.dy - 16,
+      width: 24,
+      height: 24,
+      child: Icon(Icons.location_on, color: color),
+    );
+  }
+
+  void showMap() {
+    showDialog(
+      context: context,
+      builder: (context) => flutter_map.MapLayoutBuilder(
+        controller: controller,
+        builder: (context, transformer) {
+          final markerPositions =
+              markers.map(transformer.fromLatLngToXYCoords).toList();
+
+          final markerWidgets = markerPositions.map(
+            (pos) => _buildMarkerWidget(pos, Colors.red),
+          );
+
+          final homeLocation =
+              transformer.fromLatLngToXYCoords(LatLng(35.68, 51.412));
+
+          final homeMarkerWidget =
+              _buildMarkerWidget(homeLocation, Colors.black);
+
+          final centerLocation = Offset(
+              transformer.constraints.biggest.width / 2,
+              transformer.constraints.biggest.height / 2);
+
+          final centerMarkerWidget =
+              _buildMarkerWidget(centerLocation, Colors.purple);
+
+          return GestureDetector(
+            onDoubleTap: _onDoubleTap,
+            onScaleStart: _onScaleStart,
+            onScaleUpdate: _onScaleUpdate,
+            onTapUp: (details) {
+              final location =
+                  transformer.fromXYCoordsToLatLng(details.localPosition);
+
+              final clicked = transformer.fromLatLngToXYCoords(location);
+
+              print('${location.longitude}, ${location.latitude}');
+              print('${clicked.dx}, ${clicked.dy}');
+              print('${details.localPosition.dx}, ${details.localPosition.dy}');
+            },
+            child: Stack(
+              children: [
+                flutter_map.Map(
+                  controller: controller,
+                  builder: (context, x, y, z) {
+                    //Legal notice: This url is only used for demo and educational purposes. You need a license key for production use.
+
+                    //Google Maps
+                    final url =
+                        'https://www.google.com/maps/vt/pb=!1m4!1m3!1i$z!2i$x!3i$y!2m3!1e0!2sm!3i420120488!3m7!2sen!5e1105!12m4!1e68!2m2!1sset!2sRoadmap!4e0!5m1!1e0!23i4111425';
+
+                    //Mapbox Streets
+                    // final url =
+                    //     'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/$z/$x/$y?access_token=YOUR_MAPBOX_ACCESS_TOKEN';
+
+                    return CachedNetworkImage(
+                      imageUrl: url,
+                      fit: BoxFit.cover,
+                    );
+                  },
+                ),
+                homeMarkerWidget,
+                ...markerWidgets,
+                centerMarkerWidget,
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
